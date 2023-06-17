ff710a1efb720f00af720ee0c11683a3c0da062d
weihsiang120 <weihsiang120@gmail.com>
2023-06-17 15:38:14 +0800

    resize upload image

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,9 @@
 
 class Product < ApplicationRecord
   has_many_attached :pictures
+
+  def picture_as_thumbnail(pic)
+    return unless pic.content_type.in?(%W[image/jpeg image/png])
+    pic.variant(resize_to_limit: [200, 200]).processed
+  end
 end
