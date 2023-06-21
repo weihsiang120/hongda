e4c4c0ff10b2baefc279e6ce8d09cc83a11bab05
weihsiang120 <weihsiang120@gmail.com>
2023-06-21 17:31:15 +0800

    remove picture with turbo_stream

--- app/controllers/products/pictures_controller.rb
+++ app/controllers/products/pictures_controller.rb
@@ -1,18 +1,19 @@
 class Products::PicturesController < ApplicationController
+  include ActionView::RecordIdentifier
   before_action :set_product
 
   def destroy
-    if picture = @product.pictures.find(params[:blob_id])
-      picture.purge_later
-      redirect_to edit_product_path(@product)
-    else
-      render edit_product_path(@product)
+    @picture = @product.pictures.find(params[:blob_id])
+    @picture.purge_later
+    respond_to do |format|
+      format.html { redirect_to edit_product_path(@product) }
+      format.turbo_stream { render turbo_stream: turbo_stream.remove(dom_id(@picture, :picture))}
     end
   end
 
   private
 
   def set_product
-    @product = Product.find(params[:id])
+    @product = Product.find(params[:product_id])
   end
 end
